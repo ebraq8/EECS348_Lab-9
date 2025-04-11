e5f4902cc2bdcaf50b2554ce0df2533f3eebe9a1
ebraq8 <ebraheemalaamer95@gmail.com>
2025-04-10 22:47:35 -0500

    Restore full Lab 9 implementation

--- main.cpp
+++ main.cpp
@@ -1,1 +1,84 @@
-// Trigger workflow run
+#include "Matrix.hpp"
+#include "utils.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: ./matrix <filename>\n";
+        return 1;
+    }
+
+    std::ifstream infile(argv[1]);
+    if (!infile) {
+        std::cerr << "Error: Cannot open file.\n";
+        return 1;
+    }
+
+    int n, typeFlag;
+    infile >> n >> typeFlag;
+
+    if (typeFlag == 0) {
+        Matrix<int> m1(n), m2(n);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                infile >> m1[i][j];
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                infile >> m2[i][j];
+
+        std::cout << "Matrix A:\n"; m1.print();
+        std::cout << "Matrix B:\n"; m2.print();
+
+        Matrix<int> sum = m1 + m2;
+        std::cout << "A + B:\n"; sum.print();
+
+        Matrix<int> product = m1 * m2;
+        std::cout << "A * B:\n"; product.print();
+
+        std::cout << "Main diagonal sum of A: " << m1.diagonalSum(true) << std::endl;
+        std::cout << "Secondary diagonal sum of A: " << m1.diagonalSum(false) << std::endl;
+
+        std::cout << "Swapping rows 0 and 1 in A:\n";
+        swapRows(m1[0], m1[1]); m1.print();
+
+        std::cout << "Swapping columns 0 and 1 in A:\n";
+        swapCols(m1, 0, 1); m1.print();
+
+        std::cout << "Updating A[0][0] to 100:\n";
+        updateElement(m1, 0, 0, 100); m1.print();
+
+    } else {
+        Matrix<double> m1(n), m2(n);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                infile >> m1[i][j];
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                infile >> m2[i][j];
+
+        std::cout << "Matrix A:\n"; m1.print();
+        std::cout << "Matrix B:\n"; m2.print();
+
+        Matrix<double> sum = m1 + m2;
+        std::cout << "A + B:\n"; sum.print();
+
+        Matrix<double> product = m1 * m2;
+        std::cout << "A * B:\n"; product.print();
+
+        std::cout << "Main diagonal sum of A: " << m1.diagonalSum(true) << std::endl;
+        std::cout << "Secondary diagonal sum of A: " << m1.diagonalSum(false) << std::endl;
+
+        std::cout << "Swapping rows 0 and 1 in A:\n";
+        swapRows(m1[0], m1[1]); m1.print();
+
+        std::cout << "Swapping columns 0 and 1 in A:\n";
+        swapCols(m1, 0, 1); m1.print();
+
+        std::cout << "Updating A[0][0] to 100.5:\n";
+        updateElement(m1, 0, 0, 100.5); m1.print();
+    }
+
+    return 0;
+}
---
+++ matrix.hpp
@@ -0,0 +1,59 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <stdexcept>
+
+template<typename T>
+class Matrix {
+private:
+    std::vector<std::vector<T>> data;
+    int size;
+
+public:
+    Matrix(int n) : size(n), data(n, std::vector<T>(n)) {}
+
+    int getSize() const { return size; }
+
+    std::vector<T>& operator[](int index) { return data[index]; }
+    const std::vector<T>& operator[](int index) const { return data[index]; }
+
+    void print() const {
+        for (const auto& row : data) {
+            for (const auto& val : row) {
+                std::cout << std::setw(8) << static_cast<double>(val);
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    friend Matrix<T> operator+(const Matrix<T>& a, const Matrix<T>& b) {
+        if (a.size != b.size) throw std::invalid_argument("Matrix sizes must match");
+        Matrix<T> result(a.size);
+        for (int i = 0; i < a.size; ++i)
+            for (int j = 0; j < a.size; ++j)
+                result[i][j] = a[i][j] + b[i][j];
+        return result;
+    }
+
+    friend Matrix<T> operator*(const Matrix<T>& a, const Matrix<T>& b) {
+        if (a.size != b.size) throw std::invalid_argument("Matrix sizes must match");
+        Matrix<T> result(a.size);
+        for (int i = 0; i < a.size; ++i)
+            for (int j = 0; j < a.size; ++j)
+                for (int k = 0; k < a.size; ++k)
+                    result[i][j] += a[i][k] * b[k][j];
+        return result;
+    }
+
+    T diagonalSum(bool mainDiagonal = true) const {
+        T sum = 0;
+        for (int i = 0; i < size; ++i)
+            sum += mainDiagonal ? data[i][i] : data[i][size - 1 - i];
+        return sum;
+    }
+};
+
+#endif
